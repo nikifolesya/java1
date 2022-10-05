7aba3def1326600caa9a0700f8276b2117b8d161
Olesya Nikiforova <olesya.nikiforova21@yandex.ru>
2022-10-05 16:32:09 +0300

    add gitignore

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -1,3 +1,4 @@
+package lab1;
 public class Palindrome {
     public static void main(String[] args) { 
         for (int i = 0; i < args.length; i++) {
--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,3 +1,4 @@
+package lab1;
 public class Primes {
     public static void main(String[] args) {
         for(int i = 2; i < 100 ; i++) {
